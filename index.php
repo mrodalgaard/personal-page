8aa23eded88bf4c9133942fe3c7d7d6f5cce1be4
Martin Rodalgaard <mrodalgaard@gmail.com>
2012-03-03 17:26:23 +0100

    working php landing page, just for a test

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php ?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>title</title>
+	<meta http-equiv="pragma" content="no-cache" />
+</head>
+
+<body>
+	
+	<div>
+		<p>Just a test of the first commit to Github and php on the apache2 server</p>
+		<p><?php echo "PHP IS WORKING, though not tested thoroughly!"?></p>
+	</div>
+
+</body>	
+</html>
