35e6ae446f2d5042966ee361f3e275ae6e24c730
Martin Rodalgaard <mrodalgaard@gmail.com>
2019-12-21 22:53:05 +0100

    Fix date unit test error

--- src/components/Age/index.test.tsx
+++ src/components/Age/index.test.tsx
@@ -1,34 +1,30 @@
-import { mount } from 'enzyme';
-import * as React from 'react';
-import { act } from 'react-dom/test-utils';
-import Age from '.';
+import { mount } from "enzyme";
+import * as React from "react";
+import { act } from "react-dom/test-utils";
+import Age from ".";
 
-describe('Age', () => {
-  let tenYearsAgo: string;
-
-  beforeEach(() => {
-    const date = new Date();
-    date.setFullYear(date.getFullYear() - 10);
-    tenYearsAgo = `${date.getMonth()}-${date.getDate()}-${date.getFullYear()}`;
-  });
-
-  it('renders', () => {
-    const wrapper = mount(<Age birthday={tenYearsAgo} updateInterval={1000} />);
-    expect(wrapper.text()).toBe('0');
+describe("Age", () => {
+  it("renders", () => {
+    const wrapper = mount(<Age birthday="02-06-2000" updateInterval={1000} />);
+    expect(wrapper.text()).toBe("0");
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('renders final result', () => {
+  it("renders final result", () => {
+    const date = new Date();
+    date.setFullYear(date.getFullYear() - 10);
+    const tenYearsAgo = `${date.getMonth()}-${date.getDate()}-${date.getFullYear()}`;
+
     jest.useFakeTimers();
 
     const wrapper = mount(<Age birthday={tenYearsAgo} updateInterval={1} />);
-    expect(wrapper.text()).toBe('0');
+    expect(wrapper.text()).toBe("0");
 
     act(() => {
       jest.runTimersToTime(20);
     });
 
-    expect(wrapper.update().text()).toBe('10');
+    expect(wrapper.update().text()).toBe("10");
     wrapper.unmount();
   });
 });
