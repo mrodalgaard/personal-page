d2fee0dd76447068131dc6dc75e4732f5e6b28cd
Martin Rodalgaard <mrodalgaard@gmail.com>
2020-10-18 23:03:15 +0200

    Move letter in on bigger medias

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -8,9 +8,13 @@ import AppProvider from './AppProvider';
 const Content = styled.div`
   margin: 20px;
 
-  @media (min-width: 500px) {
+  @media (min-width: 550px) {
     float: right;
     width: 400px;
+  }
+
+  @media (min-width: 1800px) {
+    margin-right: 15%;
   }
 `;
 
